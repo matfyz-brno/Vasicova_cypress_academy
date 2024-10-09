00748318723a997e69416573243ddf6f5467545c
Eva Vašicová <miev@seznam.cz>
2024-10-09 20:58:57 +0200

    new cypress config

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    watchForFileChanges: false, //zaridi, ze se nebude cypress restartovat po kazde zmene
+    defaultCommandTimeout: 10000, //zmena time out
+  },
+});
