a6ab0a3cf39f8d63086eee097a7b4500e25139a7
Eva Vašicová <miev@seznam.cz>
2024-10-24 16:38:32 +0200

    Send lost password, two type of tests

--- cypress/e2e/bonus-homeworks/lost_password_tests.cy.js
+++ cypress/e2e/bonus-homeworks/lost_password_tests.cy.js
@@ -1,16 +1,28 @@
 import { LoginPage } from "../../page-objects/pmtool/login_page";
+import { faker } from "@faker-js/faker";
 describe("Lost password", () => {
   it("Lost password for random user", () => {
+    const fakeLostUserName = faker.internet.userName();
+    const fakeLostEmail = faker.internet.exampleEmail({
+      firstName: fakeLostUserName,
+    });
     new LoginPage()
       .openPmtool()
       .clickLostPassword()
-      .typeLostUserName("dgdgf")
-      .typeLostEmail("dgdgd@sfsfsd.cz")
+      .typeLostUserName(fakeLostUserName)
+      .typeLostEmail(fakeLostEmail)
       .clickSendBtn()
       .clickBackBtn();
   });
 
-  /*it.skip("Lost password for known user", () => {
-    new LoginPage().openPmtool();
-  });*/
+  it("Lost password for known user", () => {
+    const fakeLostUserName = "lost_password_user";
+    const fakeLostEmail = "lost_password@tredgate.cz";
+    new LoginPage()
+      .openPmtool()
+      .clickLostPassword()
+      .typeLostUserName(fakeLostUserName)
+      .typeLostEmail(fakeLostEmail)
+      .clickSendBtn();
+  });
 });
