16089dc266705cabf496552afa70d638c39b2e64
Eva Vašicová <miev@seznam.cz>
2024-11-10 23:11:25 +0100

    Create prototype of test

---
+++ cypress/e2e/homeworks/AutomationTestStore_login_test.cy.js
@@ -0,0 +1,46 @@
+import { faker } from "@faker-js/faker";
+import { HomePageAutomationStore } from "../../page-objects/homeworks/AutomationTestStore-home_page";
+describe("Automation Test Store", () => {
+  it("Fill in form", () => {
+    const fakeFirstName = faker.person.firstName();
+    const fakeLastName = faker.person.lastName();
+    const fakeEmail = faker.internet.exampleEmail({
+      firstName: fakeFirstName,
+      lastName: fakeLastName,
+    });
+    const fakePhone = faker.phone.number();
+    const fakeFax = faker.phone.number();
+    const fakeCompany = faker.company.name();
+    const fakeAddress1 = faker.location.streetAddress();
+    const fakeAddress2 = faker.location.streetAddress();
+    const fakeCity = faker.location.city();
+    const fakeZipCode = faker.location.zipCode();
+    const fakeLoginName = faker.internet.userName({
+      firstName: fakeFirstName,
+      lastName: fakeLastName,
+    });
+    const fakePassword = faker.internet.password();
+    new HomePageAutomationStore()
+      .openStorePage()
+      .openLoginPage()
+      .openNewCustomerPage()
+      .typeFirstName(fakeFirstName)
+      .typeLastName(fakeLastName)
+      .typeEmail(fakeEmail)
+      .typePhone(fakePhone)
+      .typeFax(fakeFax)
+      .typeCompany(fakeCompany)
+      .typeAddress1(fakeAddress1)
+      .typeAddress2(fakeAddress2)
+      .typeCity(fakeCity)
+      .typeZipCode(fakeZipCode)
+      .selectCountry()
+      .selectRegion()
+      .typeLoginName(fakeLoginName)
+      .typePassword(fakePassword)
+      .typePasswordConfirm(fakePassword)
+      .clickSubscribe()
+      .clickPrivacyPolicy()
+      .submitForm();
+  });
+});
--- cypress/page-objects/homeworks/AutomationTestStore-new_customer_page.js
+++ cypress/page-objects/homeworks/AutomationTestStore-new_customer_page.js
@@ -3,5 +3,121 @@ import { HeaderSectionAutomationStore } from "./common/AutomationTestStore-heade
 export class NewCustomerPageAutomationStore extends HeaderSectionAutomationStore {
   constructor() {
     super();
+    this.form = "#AccountFrm";
+    this.firstNameInput = "#AccountFrm_firstname";
+    this.lastNameInput = "#AccountFrm_lastname";
+    this.emailInput = "#AccountFrm_email";
+    this.phoneInput = "#AccountFrm_telephone";
+    this.faxInput = "#AccountFrm_fax";
+    this.companyInput = "#AccountFrm_company";
+    this.address1Input = "#AccountFrm_address_1";
+    this.address2Input = "#AccountFrm_address_2";
+    this.cityInput = "#AccountFrm_city";
+    this.regionInput = "#AccountFrm_zone_id";
+    this.zipCodeInput = "#AccountFrm_postcode";
+    this.countryInput = "#AccountFrm_country_id";
+    this.loginNameInput = "#AccountFrm_loginname";
+    this.passwordInput = "#AccountFrm_password";
+    this.passwordConfirmInput = "#AccountFrm_confirm";
+    this.subscribeRadioBtn = "#AccountFrm_newsletter0";
+    this.privacyPolicyCheckbox = "#AccountFrm_agree";
+    this.continueBtn = "button[title='Continue']";
+    this.positiveSubmitMessage = ".maintext";
+  }
+  typeFirstName(randomFirstName) {
+    cy.get(this.firstNameInput).type(randomFirstName);
+    return this;
+  }
+
+  typeLastName(randomLastName) {
+    cy.get(this.lastNameInput).type(randomLastName);
+    return this;
+  }
+
+  typeEmail(randomEmail) {
+    cy.get(this.emailInput).type(randomEmail);
+    return this;
+  }
+
+  typePhone(randomPhone) {
+    cy.get(this.phoneInput).type(randomPhone);
+    return this;
+  }
+
+  typeFax(randomFax) {
+    cy.get(this.faxInput).type(randomFax);
+    return this;
+  }
+
+  typeCompany(randomCompany) {
+    cy.get(this.companyInput).type(randomCompany);
+    return this;
+  }
+
+  typeAddress1(randomAddress1) {
+    cy.get(this.address1Input).type(randomAddress1);
+    return this;
+  }
+
+  typeAddress2(randomAddress2) {
+    cy.get(this.address2Input).type(randomAddress2);
+    return this;
+  }
+
+  typeCity(randomCity) {
+    cy.get(this.cityInput).type(randomCity);
+    return this;
+  }
+
+  selectRegion() {
+    cy.get(this.regionInput).select("Vysocina");
+    cy.get(this.regionInput).should("have.value", "901");
+    return this;
+  }
+
+  typeZipCode(randomZipCode) {
+    cy.get(this.zipCodeInput).type(randomZipCode);
+    return this;
+  }
+
+  selectCountry() {
+    cy.get(this.countryInput).select("Czech Republic");
+    cy.get(this.countryInput).should("have.value", "56");
+    return this;
+  }
+
+  typeLoginName(randomLoginName) {
+    cy.get(this.loginNameInput).type(randomLoginName);
+    return this;
+  }
+
+  typePassword(randomPassword) {
+    cy.get(this.passwordInput).type(randomPassword);
+    return this;
+  }
+
+  typePasswordConfirm(randomPassword) {
+    cy.get(this.passwordConfirmInput).type(randomPassword);
+    return this;
+  }
+
+  clickSubscribe() {
+    cy.get(this.subscribeRadioBtn).check();
+    cy.get(this.subscribeRadioBtn).should("be.checked");
+    return this;
+  }
+
+  clickPrivacyPolicy() {
+    cy.get(this.privacyPolicyCheckbox).check();
+    cy.get(this.privacyPolicyCheckbox).should("be.checked");
+    return this;
+  }
+
+  submitForm() {
+    cy.get(this.form).submit();
+    cy.get(this.positiveSubmitMessage)
+      .should("be.visible")
+      .and("contain.text", "Your Account Has Been Created!");
+    return this;
   }
 }
